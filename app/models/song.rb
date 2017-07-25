311719d0deda89789156ea77fef8b5e52ac8da9c
Benjamin-Coleman <github email address>
2017-07-25 11:33:08 -0400

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,5 @@
+class Song < ActiveRecord::Base
+	validates :title, presence: true
+	validates :title, uniqueness: { scope: [:release_year, :artist_name]}
+	validates :release_year, numericality: {less_than_or_equal_to: 2017, only_integer: true}, if: :released
+end
